5499987f2a90b621c9afcaefbd6d6df6d125332d
Daffa Fathani A <daffa26@gmail.com>
2021-05-29 18:03:57 +0800

    finished login page

---
+++ lib/login.dart
@@ -0,0 +1,207 @@
+import "package:flutter/material.dart";
+
+class MyHomeApp extends StatefulWidget {
+  @override
+  _MyHomeAppState createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+        children: [
+          Container(
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    "Team",
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(25.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    "Up",
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(150.0, 175.0, 0.0, 0.0),
+                  child: Text(
+                    "!",
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Montserrat"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 0.0),
+            child: Column(
+              children: [
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "EMAIL",
+                          labelStyle: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "PASSWORD",
+                          labelStyle: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  alignment: Alignment(1, 0),
+                  child: InkWell(
+                    onTap: () => print("tapped"),
+                    child: Text(
+                      "Forgot Password",
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 0.0),
+          Container(
+            height: 50,
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+            child: GestureDetector(
+              onTap: () => print("logged in"),
+              child: Material(
+                color: Colors.purple.shade200,
+                borderRadius: BorderRadius.circular(25.0),
+                elevation: 7.0,
+                child: Center(
+                  child: Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+            color: Colors.transparent,
+            child: GestureDetector(
+              onTap: () => print("logged in"),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple.shade200,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(25)),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Image.asset("assets/images/google.png"),
+                      ),
+                      SizedBox(width: 15),
+                      Center(
+                        child: Text(
+                          "LOG IN WITH GOOGLE",
+                          style: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.w700,
+                            fontSize: 15,
+                            color: Colors.purple.shade200,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account yet?",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontFamily: "Montserrat",
+                  ),
+                ),
+                InkWell(
+                  onTap: () => print("tapped"),
+                  child: Text(
+                    " Sign Up",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontFamily: "Montserrat",
+                      color: Colors.purple,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "singnup.dart";
 
 void main() {
   runApp(MyApp());
@@ -10,116 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomeApp(),
-    );
-  }
-}
-
-class MyHomeApp extends StatefulWidget {
-  @override
-  _MyHomeAppState createState() => _MyHomeAppState();
-}
-
-class _MyHomeAppState extends State<MyHomeApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            child: Stack(
-              children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text(
-                    "Team",
-                    style: TextStyle(
-                        fontSize: 80.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Montserrat"),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(25.0, 175.0, 0.0, 0.0),
-                  child: Text(
-                    "Up",
-                    style: TextStyle(
-                        fontSize: 80.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Montserrat"),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(150.0, 175.0, 0.0, 0.0),
-                  child: Text(
-                    "!",
-                    style: TextStyle(
-                        color: Colors.purple,
-                        fontSize: 80.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Montserrat"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(20.0, 35.0, 20.0, 0.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "EMAIL",
-                    labelStyle: TextStyle(
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "PASSWORD",
-                    labelStyle: TextStyle(
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.purple),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  alignment: Alignment(1, 0),
-                  child: InkWell(
-                    onTap: () => print("tapped"),
-                    child: Text(
-                      "Forgot Password",
-                      style: TextStyle(
-                        color: Colors.purple,
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
     );
   }
 }
