8aacdb7abb09a6bff14373b14a46f64da299e14c
Daffa Fathani A <daffa26@gmail.com>
2021-07-23 07:32:40 +0800

    update UI and fixed bugs

--- lib/services/googleAuth.dart
+++ lib/services/googleAuth.dart
@@ -3,18 +3,19 @@ import "package:google_sign_in/google_sign_in.dart";
 
 Future<UserCredential> signInWithGoogle() async {
   // Trigger the authentication flow
+  print("1");
   final GoogleSignInAccount googleUser =
       await GoogleSignIn().signIn() as GoogleSignInAccount;
-
+  print("2");
   // Obtain the auth details from the request
   final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
+  print("3");
   // Create a new credential
   final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth.accessToken,
     idToken: googleAuth.idToken,
   );
-
+  print("4");
   // Once signed in, return the UserCredential
   return await FirebaseAuth.instance.signInWithCredential(credential);
 }
